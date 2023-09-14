97af0ad43f69f6ce2d23c6096f06132652bfddac
pupperemeritus <pupperemeritus9@protonmail.com>
2023-09-15 00:15:00 +0530

    components 2

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import About from './about';
+import Contact from './contact';
+import Footer from './footer';
+import Hero from './hero';
+import Navbar from './navbar';
+import Mentors from './mentors';
+import Sponsors from './sponsors';
+import Timeline from './timeline';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
