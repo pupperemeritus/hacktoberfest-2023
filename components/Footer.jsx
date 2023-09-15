22b1c2dc138aa232e4072cb891864ee378b05497
Riteshgarlapati <riteshgarlapati20@gmail.com>
2023-09-15 15:12:59 +0530

    footer and header

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+// import insta from '/images/insta.svg'
+// import facebook from '/images/facebook.svg'
+// import twitter from '/images/twitter.svg'
+// import linkedin from '/images/linkedin.svg'
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+function footer(){
+    return(
+        <div>
+            <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+            <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+            <div>
+                <h4>COSC is an open source focused tech community based in Chaitanya Bharathi Institute of Technology - Hyderabad. We conduct hackathons, bootcamps and workshops to teach students different technologies while promoting open source culture.</h4>
+            </div>
+            <div>
+                <a href="https://www.facebook.com/cbitosc/"><img className="logo"src={facebook}/></a>
+                <a href="https://twitter.com/cbitosc?t=rWGqPsaidSTGfA8FpwPPUw&s=09"><img className="logo"src={twitter}/></a>
+                <a href="https://www.instagram.com/cbitosc/"><img className="logo"src={insta}/></a>
+                <a href="https://www.linkedin.com/company/cbitosc/"><img className="logo"src={linkedin}/></a>
+            </div>
+            <div>
+                <a className="foot" href="https://hacktoberfest.com/">Hacktober Fest</a>
+            </div>
+            <div>
+                <a className="foot" href="https://cbitosc.github.io/">CBIT Open Source Community</a>
+            </div>
+            <div>
+                <a className="foot" href="https://cbitosc.github.io/coc/">Code Of Counduct</a>
+            </div>
+        </div>
+    )
+}
 
-export default Footer;
+export default footer
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react';
+import im from '/images/HFest23_Logo.png' ;
 
-const Hero = () => {
-  return <div>Hero</div>;
-};
+const Hero=()=>{
+    return (
+        <div>
+            <div>
+        <img
+          width="300px"
+          height="120px"
+          src={im}
+          alt="Logo"
+        />
+      </div>
+      <div>
+        <h1>Welcome to COSC Hacktoberfest Hackathon'23</h1>
+        <h5>The Biggest Technical Fest of CBIT<br />7th and 8th October,2023</h5>
+        <a href="https://www.instagram.com/">Register!</a>
+      </div>
+        </div>
+    )
+}
 
-export default Hero;
+export default Hero
