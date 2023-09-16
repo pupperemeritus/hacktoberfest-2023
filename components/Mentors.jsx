a8f9afc3089607b295b9c61d2af1c98ba0578acd
Akil <130962883+akil-krsna@users.noreply.github.com>
2023-09-16 16:58:01 +0530

    Updated Mentors.jsx components

--- components/Mentors.jsx
+++ components/Mentors.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React from 'react';
 
-const Mentors = () => {
-  return <div>Mentors</div>;
-};
+function Mentors() {
+  const panelists = [
+    {
+      name: 'Panelist 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non diam nec nisl commodo semper.',
+    },
+    {
+      name: 'Panelist 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non diam nec nisl commodo semper.',
+    },
+    {
+      name: 'Panelist 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non diam nec nisl commodo semper.',
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Panelists</h1>
+      <div className="panelist-list">
+        {panelists.map((panelist, index) => (
+          <div key={index} className="panelist">
+            <h2>{panelist.name}</h2>
+            <p>{panelist.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Mentors;
+
