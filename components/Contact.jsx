01c6598fe986e5428b8419f8390d4779689a53ee
Riteshgarlapati <riteshgarlapati20@gmail.com>
2023-09-18 00:27:43 +0530

    contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,18 +7,24 @@ const Contact = () => {
 
   return (
     <div className="mx-auto p-4 sm:w-1/2">
+      <div className=" flex flex-col items-center gap-2">
+        <h1 className=" text-center text-3xl text-bblue-200">
+          Contact Information
+        </h1>
+        <div className=" h-[2px] bg-bblue-200 w-16"></div>
+      </div>
       <div className="bg-gray-200 rounded-lg p-6 ">
-        <h2 className="text-lg font-semibold text-center mb-4">Contact Information</h2>
+        {/* <h2 className="text-lg font-semibold text-center mb-4">Contact Information</h2> */}
         <div className="border border-solid border-bgold-200 p-3 rounded-md mt-3">
-          <h3 className="text-md font-medium">Location:</h3>
+          <h3 className="text-md font-medium text-bblue-100">Location:</h3>
           <p className="text-gray-700">{location}</p>
         </div>
         <div className="border border-solid border-bgold-200 p-3 rounded-md mt-3">
-          <h3 className="text-md font-medium">Email:</h3>
+          <h3 className="text-md font-medium text-bblue-100">Email:</h3>
           <p className="text-gray-700">{email}</p>
         </div>
         <div className="border border-solid border-bgold-200 p-3 rounded-md mt-3">
-          <h3 className="text-md font-medium">Phone:</h3>
+          <h3 className="text-md font-medium  text-bblue-100">Phone:</h3>
           <p className="text-gray-700">{phone}</p>
         </div>
       </div>
