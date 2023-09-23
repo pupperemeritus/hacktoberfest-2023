0e5f79aea1724e41d21622ce64839447196853b6
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-09-23 23:29:37 +0530

    Changed X Logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,12 +6,27 @@ function Footer() {
     <div className=" w-[85%] mx-auto mb-16 ">
       <div>
         <p className="text-center text-Manga-100">
-
-<br /><br /><strong>What We Do</strong><br /><br />
-At COSC, we're more than just an event organizer; we're a community dedicated to advancing the open source culture. We host hackathons, conduct bootcamps, and facilitate workshops to equip students with the skills they need to thrive in the world of technology. Through our events, we nurture creativity, collaboration, and innovation.
-
-<br /><br /><strong>Join Us on This Journey</strong><br /><br />
-HacktoberFest isn't just an event; it's a chance to be part of something bigger, to embrace the power of open source, and to make an impact in the tech world. So, whether you're a seasoned developer, a curious beginner, or simply someone with a passion for technology, we invite you to join us on this exhilarating journey. Let's learn, code, and celebrate the magic of open source together!
+          <br />
+          <br />
+          <strong>What We Do</strong>
+          <br />
+          <br />
+          At COSC, we're more than just an event organizer; we're a community
+          dedicated to advancing the open source culture. We host hackathons,
+          conduct bootcamps, and facilitate workshops to equip students with the
+          skills they need to thrive in the world of technology. Through our
+          events, we nurture creativity, collaboration, and innovation.
+          <br />
+          <br />
+          <strong>Join Us on This Journey</strong>
+          <br />
+          <br />
+          HacktoberFest isn't just an event; it's a chance to be part of
+          something bigger, to embrace the power of open source, and to make an
+          impact in the tech world. So, whether you're a seasoned developer, a
+          curious beginner, or simply someone with a passion for technology, we
+          invite you to join us on this exhilarating journey. Let's learn, code,
+          and celebrate the magic of open source together!
         </p>
       </div>
       <div className=" self-center w-fit mx-auto my-8">
@@ -20,7 +35,7 @@ HacktoberFest isn't just an event; it's a chance to be part of something bigger,
             <Image src="/images/facebook.svg" width={40} height={40} />
           </Link>
           <Link href="https://twitter.com/cbitosc?t=rWGqPsaidSTGfA8FpwPPUw&s=09">
-            <Image src="/images/twitter.svg" width={40} height={40} />
+            <Image src="/images/x.png" width={40} height={40} />
           </Link>
           <Link href="https://www.instagram.com/cbitosc/">
             <Image src="/images/insta.svg" width={40} height={40} />
