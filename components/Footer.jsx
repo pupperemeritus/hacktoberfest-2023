90f3008885a08619c4c15b9b24f9c86c9cfb8609
Akil <130962883+akil-krsna@users.noreply.github.com>
2023-09-23 23:02:36 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,10 +6,12 @@ function Footer() {
     <div className=" w-[85%] mx-auto mb-16 ">
       <div>
         <p className="text-center text-Manga-100">
-          COSC is an open source focused tech community based in Chaitanya
-          Bharathi Institute of Technology - Hyderabad. We conduct hackathons,
-          bootcamps and workshops to teach students different technologies while
-          promoting open source culture.
+
+<br /><br /><strong>What We Do</strong><br /><br />
+At COSC, we're more than just an event organizer; we're a community dedicated to advancing the open source culture. We host hackathons, conduct bootcamps, and facilitate workshops to equip students with the skills they need to thrive in the world of technology. Through our events, we nurture creativity, collaboration, and innovation.
+
+<br /><br /><strong>Join Us on This Journey</strong><br /><br />
+HacktoberFest isn't just an event; it's a chance to be part of something bigger, to embrace the power of open source, and to make an impact in the tech world. So, whether you're a seasoned developer, a curious beginner, or simply someone with a passion for technology, we invite you to join us on this exhilarating journey. Let's learn, code, and celebrate the magic of open source together!
         </p>
       </div>
       <div className=" self-center w-fit mx-auto my-8">
