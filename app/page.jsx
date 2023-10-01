45c23747b5bc49bdb4b04c67754e2297aff93ce9
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-10-01 21:38:29 +0530

    added sponsors to navbar

--- app/page.jsx
+++ app/page.jsx
@@ -8,34 +8,40 @@ import Sponsors from "@/components/Sponsors";
 import TimelineMain from "@/components/TimelineCon";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <div className=" spacer layer1">
-        <section id="home" className="  section">
-          <Hero />
-        </section>
-        <section
-          id="about"
-          className=" section justify-between w-full md:mt-6 pt-10 h-100"
-        >
-          <About />
-        </section>
-      </div>
+    return (
+        <main className="flex flex-col items-center justify-between min-h-screen ">
+            <div className=" spacer layer1">
+                <section id="home" className=" section">
+                    <Hero />
+                </section>
+                <section
+                    id="about"
+                    className="justify-between w-full pt-10 section md:mt-6 h-100"
+                >
+                    <About />
+                </section>
+            </div>
 
-      {/* <div className="spacer layer2"></div> */}
-      <section id="schedule" className=" spacer layer2 w-full section relative">
-        <TimelineMain />
-      </section>
+            {/* <div className="spacer layer2"></div> */}
+            <section
+                id="schedule"
+                className="relative w-full spacer layer2 section"
+            >
+                <TimelineMain />
+            </section>
 
-      <div className=" spacer layer5 flex flex-col justify-center w-full mb-12">
-        <section id="mentors" className=" section pt-16">
-          <Mentors />
-        </section>
-        <Sponsors />
-        <section id="contact" className=" section w-full pt-28">
-          <Contact />
-        </section>
-      </div>
-    </main>
-  );
+            <div className="flex flex-col justify-center w-full mb-12 spacer layer5">
+                <section id="mentors" className="pt-16 section">
+                    <Mentors />
+                </section>
+                <section id="sponsors" className="pt-16 section">
+                    <Sponsors />
+                </section>
+
+                <section id="contact" className="w-full section pt-28">
+                    <Contact />
+                </section>
+            </div>
+        </main>
+    );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,207 +6,218 @@ import Link from "next/link";
 import Image from "next/image"; // Import Image from Next.js
 
 const Navbar = () => {
-  const Links = [
-    { name: "Home", link: "/", section: "home" },
-    { name: "About", link: "/#about", section: "about" },
-    { name: "Schedule", link: "/#schedule", section: "schedule" },
-    { name: "Mentors", link: "/#mentors", section: "mentors" },
-    { name: "Contact", link: "/#contact", section: "contact" },
-    { name: "Register", link: "/register", section: "register" },
-  ];
+    const Links = [
+        { name: "Home", link: "/", section: "home" },
+        { name: "About", link: "/#about", section: "about" },
+        { name: "Schedule", link: "/#schedule", section: "schedule" },
+        { name: "Mentors", link: "/#mentors", section: "mentors" },
+        { name: "Sponsors", link: "/#sponsors", section: "sponsors" },
+        { name: "Contact", link: "/#contact", section: "contact" },
+        { name: "Register", link: "/register", section: "register" },
+    ];
 
-  const [activeSection, setActiveSection] = useState("home");
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const [isSticky, setIsSticky] = useState(false);
+    const [activeSection, setActiveSection] = useState("home");
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const [isSticky, setIsSticky] = useState(false);
 
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  // Helper function to get the active section/page
-  const getActiveSection = () => {
-    const sections = document.querySelectorAll(".section");
-    for (const section of sections) {
-      const sectionTop = section.getBoundingClientRect().top - 10000;
-      const sectionBottom = section.getBoundingClientRect().bottom;
-      if (sectionTop <= 0 && sectionBottom >= 0) {
-        return section.id;
-      }
-    }
+    // Helper function to get the active section/page
+    const getActiveSection = () => {
+        const sections = document.querySelectorAll(".section");
+        for (const section of sections) {
+            const sectionTop = section.getBoundingClientRect().top - 10000;
+            const sectionBottom = section.getBoundingClientRect().bottom;
+            if (sectionTop <= 0 && sectionBottom >= 0) {
+                return section.id;
+            }
+        }
 
-    return null;
-  };
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
-
-  const scrollDownOnLoad = () => {
-    const scrollAmount = 1; // Adjust this value as needed
-    window.scrollBy(0, scrollAmount);
-  };
-
-  const handleLink = (event, link) => {
-    event.preventDefault();
-    window.location.href = link;
-    scrollDownOnLoad();
-    setMenuOpen(false);
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const activeSection = getActiveSection();
-
-      setActiveSection(activeSection);
-
-      const scrollTop = window.scrollY;
-      setIsSticky(scrollTop > 0);
+        return null;
+    };
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
     };
 
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
+    const scrollDownOnLoad = () => {
+        const scrollAmount = 1; // Adjust this value as needed
+        window.scrollBy(0, scrollAmount);
     };
-  }, []);
 
-  return (
-    <>
-      <div className="w-full bg-void px-4 text-md py-1 text-bblue-200 flex justify-center">
-        <span className="shimmer">Learn. Code. Share.</span>
-      </div>
-      <div
-        className={`${
-          isSticky
-            ? "sticky top-0 h-16 backdrop-blur-3xl bg-[#0f0913] bg-opacity-70 shadow-md navbar-ios"
-            : "bg-void"
-        } left-0 h-16 py-2 px-4 flex justify-between items-center w-full transition-all duration-300 z-10 text-2xl `}
-        style={{
-          "z-index": 2,
-        }}
-      >
-        <div className="flex items-center">
-          <Link href="/" className=" flex items-center">
-            <Image
-              src="/images/hf10iconCrop.png" // Replace with your logo image path
-              width={51}
-              height={45}
-              alt="Hf10 Logo"
-              style={{ padding: 0, scale: 1.0 }}
-            />
-            <div className="w-[1px] h-5 bg-[#efedef] mx-2"></div>
-            <div className="relative w-10 h-10">
-              {isSticky ? (
-                <div>
-                  <Image
-                    src="/images/LogoCOSCRing.svg" // Replace with your logo image path
-                    layout="fill"
-                    alt="Sticky Logo"
-                    style={{
-                      transition: "opacity 0.3s ease-in-out",
-                      opacity: 1,
-                    }}
-                  />
-                  <Image
-                    src="/images/LogoCOSC.png" // Replace with your logo image path
-                    layout="fill"
-                    alt="Normal Logo"
-                    style={{
-                      transition: "opacity 0.3s ease-in-out",
-                      opacity: 0,
-                    }}
-                  />
-                </div>
-              ) : (
-                <div>
-                  <Image
-                    src="/images/LogoCOSCRing.svg" // Replace with your logo image path
-                    layout="fill"
-                    alt="Sticky Logo"
-                    style={{
-                      transition: "opacity 0.3s ease-in-out",
-                      opacity: 0,
-                    }}
-                  />
-                  <Image
-                    src="/images/LogoCOSC.png" // Replace with your logo image path
-                    layout="fill"
-                    alt="Normal Logo"
-                    style={{
-                      transition: "opacity 0.3s ease-in-out",
-                      opacity: 1,
-                    }}
-                  />
-                </div>
-              )}
+    const handleLink = (event, link) => {
+        event.preventDefault();
+        window.location.href = link;
+        scrollDownOnLoad();
+        setMenuOpen(false);
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const activeSection = getActiveSection();
+
+            setActiveSection(activeSection);
+
+            const scrollTop = window.scrollY;
+            setIsSticky(scrollTop > 0);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <>
+            <div className="flex justify-center w-full px-4 py-1 bg-void text-md text-bblue-200">
+                <span className="shimmer">Learn. Code. Share.</span>
             </div>
-          </Link>
-        </div>
-        <div className="md:flex hidden px-4">
-          <ul className="md:flex md:items-center">
-            {Links.map((link, index) => (
-              <li
-                key={index}
-                className={`md:mx-2 ${
-                  (pathname === "/register" && link.link == "/register") ||
-                  (pathname === "/" && link.section == activeSection)
-                    ? " text-bblue-200" // Apply 'active' class to the current page or section
-                    : ""
-                }`}
-              >
-                <Link
-                  href={link.link}
-                  onClick={(event) => handleLink(event, link.link)}
-                  className=" text-xl"
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="md:hidden cursor-pointer" onClick={toggleMenu}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="white"
-          >
-            {isMenuOpen ? (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            ) : (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            )}
-          </svg>
-        </div>
-        {isMenuOpen && (
-          <div className="md:hidden absolute top-16 right-4 bg-[#0f0913] py-2 px-4 rounded-lg">
-            <ul>
-              {Links.map((link, index) => (
-                <li key={index} className="my-2 ">
-                  <Link
-                    href={link.link}
-                    onClick={(event) => handleLink(event, link.link)}
-                    className="hover:text-[#d2b863] duration-300 text-lg"
-                  >
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    </>
-  );
+            <div
+                className={`${
+                    isSticky
+                        ? "sticky top-0 h-16 backdrop-blur-3xl bg-[#0f0913] bg-opacity-70 shadow-md navbar-ios"
+                        : "bg-void"
+                } left-0 h-16 py-2 px-4 flex justify-between items-center w-full transition-all duration-300 z-10 text-2xl `}
+                style={{
+                    "z-index": 2,
+                }}
+            >
+                <div className="flex items-center">
+                    <Link href="/" className="flex items-center ">
+                        <Image
+                            src="/images/hf10iconCrop.png" // Replace with your logo image path
+                            width={51}
+                            height={45}
+                            alt="Hf10 Logo"
+                            style={{ padding: 0, scale: 1.0 }}
+                        />
+                        <div className="w-[1px] h-5 bg-[#efedef] mx-2"></div>
+                        <div className="relative w-10 h-10">
+                            {isSticky ? (
+                                <div>
+                                    <Image
+                                        src="/images/LogoCOSCRing.svg" // Replace with your logo image path
+                                        layout="fill"
+                                        alt="Sticky Logo"
+                                        style={{
+                                            transition:
+                                                "opacity 0.3s ease-in-out",
+                                            opacity: 1,
+                                        }}
+                                    />
+                                    <Image
+                                        src="/images/LogoCOSC.png" // Replace with your logo image path
+                                        layout="fill"
+                                        alt="Normal Logo"
+                                        style={{
+                                            transition:
+                                                "opacity 0.3s ease-in-out",
+                                            opacity: 0,
+                                        }}
+                                    />
+                                </div>
+                            ) : (
+                                <div>
+                                    <Image
+                                        src="/images/LogoCOSCRing.svg" // Replace with your logo image path
+                                        layout="fill"
+                                        alt="Sticky Logo"
+                                        style={{
+                                            transition:
+                                                "opacity 0.3s ease-in-out",
+                                            opacity: 0,
+                                        }}
+                                    />
+                                    <Image
+                                        src="/images/LogoCOSC.png" // Replace with your logo image path
+                                        layout="fill"
+                                        alt="Normal Logo"
+                                        style={{
+                                            transition:
+                                                "opacity 0.3s ease-in-out",
+                                            opacity: 1,
+                                        }}
+                                    />
+                                </div>
+                            )}
+                        </div>
+                    </Link>
+                </div>
+                <div className="hidden px-4 md:flex">
+                    <ul className="md:flex md:items-center">
+                        {Links.map((link, index) => (
+                            <li
+                                key={index}
+                                className={`md:mx-2 ${
+                                    (pathname === "/register" &&
+                                        link.link == "/register") ||
+                                    (pathname === "/" &&
+                                        link.section == activeSection)
+                                        ? " text-bblue-200" // Apply 'active' class to the current page or section
+                                        : ""
+                                }`}
+                            >
+                                <Link
+                                    href={link.link}
+                                    onClick={(event) =>
+                                        handleLink(event, link.link)
+                                    }
+                                    className="text-xl "
+                                >
+                                    {link.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="cursor-pointer md:hidden" onClick={toggleMenu}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="white"
+                    >
+                        {isMenuOpen ? (
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        ) : (
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h16M4 18h16"
+                            />
+                        )}
+                    </svg>
+                </div>
+                {isMenuOpen && (
+                    <div className="md:hidden absolute top-16 right-4 bg-[#0f0913] py-2 px-4 rounded-lg">
+                        <ul>
+                            {Links.map((link, index) => (
+                                <li key={index} className="my-2 ">
+                                    <Link
+                                        href={link.link}
+                                        onClick={(event) =>
+                                            handleLink(event, link.link)
+                                        }
+                                        className="hover:text-[#d2b863] duration-300 text-lg"
+                                    >
+                                        {link.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default Navbar;
