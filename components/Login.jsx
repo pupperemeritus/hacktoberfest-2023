1a21650b911c220140d7365e6803bddaceb18411
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-10-01 18:11:06 +0530

    added login functionality

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,83 +1,97 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
-import { collection, addDoc } from 'firebase/firestore';
-import { Firestore } from "@/firebase";
-import 'firebase/auth/';
-import firebase from 'firebase/app';
+import React, { useState, useEffect } from "react";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+import { signIn } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 function Login({ router }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            handleLogin();
-        }
-    };
-    const handleLogin = async () => {
-        setError(null); // Reset any previous errors
-        setIsLoading(true); // Show loading indicator
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleLogin();
+    }
+  };
+  const handleLogin = async () => {
+    setError(null); // Reset any previous errors
+    setIsLoading(true); // Show loading indicator
 
-        // const { user, error } = await supabase.auth.signIn({
-        //     email,
-        //     password,
-        // });
-        setIsLoading(false); // Hide loading indicator
+    signIn("credentials", {
+      email,
+      password,
+      redirect: true,
+      callbackUrl: "/register",
+    });
+    setIsLoading(false); // Hide loading indicator
 
-        if (error) {
-            setError(error.message); // Set the error message if there's an error
-            console.error('Login error:', error.message);
-        } else {
-            console.log('Logged in successfully:', user);
-            // Redirect or perform any other actions after successful login
-            router.push('/statements');
+    if (error) {
+      setError(error.message); // Set the error message if there's an error
+      console.error("Login error:", error.message);
+    } else {
+      console.log("Logged in successfully:", user);
+      // Redirect or perform any other actions after successful login
+      router.push("/statements");
+    }
+  };
 
-        }
-    };
-
-    return (
-        <div className=" h-full w-full mx-auto my-0 py-0 flex flex-col-reverse justify-center justify-items-center items-center">
-            <form className="lg:w-3/5 md:w-3/5 w-4/5 h-full">
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        value={email}
-                        className="form-control shadow-md md:text-xl rounded-lg bg-Manga-200 bg-opacity-20 border border-bgold-200 px-4 py-2 w-full"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="w-full justify-center h-full justify-center my-3 w-full">
-                    <label>Password:</label>
-                    <div className="flex">
-                        <input
-                            type={isPasswordVisible ? 'text' : 'password'}
-                            value={password}
-                            className="form-control shadow-md md:text-xl rounded-lg bg-Manga-200 bg-opacity-20 border border-bgold-200 px-4 py-2 w-full"
-                            onChange={(e) => setPassword(e.target.value)}
-                            onKeyDown={handleKeyPress}
-                        />
-                        <button
-                            type="button"
-                            className="cursor-pointer  bg-bblue-200 text-void text-xl rounded-lg border border-bgold-200 hover:bg-bblue-300 duration-300"
-                            onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-                        >
-                            {isPasswordVisible ? <EyeIcon className="w-10 h-10 text-gray-500" /> : <EyeSlashIcon className="w-10 h-10 text-gray-500" />}
-                        </button>
-                    </div>
-                </div>
-                <div className=" flex justify-center my-3 w-full">
-                    <button type="button" className="cursor-pointer px-4 py-2 bg-bblue-200 text-void text-xl rounded-lg border border-bgold-200 hover:bg-bblue-300 duration-300" onClick={handleLogin}>
-                        Login
-                    </button>
-                </div>
-            </form>
+  return (
+    <div className="flex flex-col-reverse items-center justify-center w-full h-full py-0 mx-auto my-0 justify-items-center">
+      <form className="w-4/5 h-full lg:w-3/5 md:w-3/5">
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            className="w-full px-4 py-2 border rounded-lg shadow-md form-control md:text-xl bg-Manga-200 bg-opacity-20 border-bgold-200"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-    );
+        <div className="justify-center w-full my-3 ">
+          <label>Password:</label>
+          <div className="flex">
+            <input
+              type={isPasswordVisible ? "text" : "password"}
+              value={password}
+              className="w-full px-4 py-2 border rounded-lg shadow-md form-control md:text-xl bg-Manga-200 bg-opacity-20 border-bgold-200"
+              onChange={(e) => setPassword(e.target.value)}
+              onKeyDown={handleKeyPress}
+            />
+            <button
+              type="button"
+              className="text-xl duration-300 border rounded-lg cursor-pointer bg-bblue-200 text-void border-bgold-200 hover:bg-bblue-300"
+              onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+            >
+              {isPasswordVisible ? (
+                <EyeIcon className="w-10 h-10 text-gray-500" />
+              ) : (
+                <EyeSlashIcon className="w-10 h-10 text-gray-500" />
+              )}
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center w-full my-3 ">
+          <button
+            type="button"
+            className="px-4 py-2 text-xl duration-300 border rounded-lg cursor-pointer bg-bblue-200 text-void border-bgold-200 hover:bg-bblue-300"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+          <button
+            type="button"
+            className="text-sm bg-[#8e4296] text-white p-2 rounded-xl"
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- firebase.js
+++ firebase.js
@@ -2,27 +2,32 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  databaseURL: process.env.NEXT_PUBLIC_databaseURL,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  measurementId: process.env.NEXT_PUBLIC_measurementId,
 };
 
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore(app);
+// const analytics = getAnalytics(app);
+// const firestore = getFirestore(app);
+const database = getDatabase(app);
+const auth = getAuth(app);
 if (app != null) {
-    console.log("Firebase initialized");
+  console.log("Firebase initialized");
 }
-export { firestore };
+export { database, auth };
