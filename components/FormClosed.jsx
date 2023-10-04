6c91565c7f8d24cc41363903cfa05fb72baf66aa
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-10-04 21:42:56 +0530

    changed wording

--- components/FormClosed.jsx
+++ components/FormClosed.jsx
@@ -5,8 +5,8 @@ const FormClosed = () => {
         <>
             <div className="px-8 py-4 mx-4 font-bold md:mx-60 my-60 glass h-min">
                 <span className="text-xl md:text-4xl">
-                    <span className=" text-bgold-200">Registrations Have</span>{" "}
-                    <span className=" text-bred-200">Ended</span>
+                    <span className=" text-bgold-200">Registrations are</span>{" "}
+                    <span className=" text-bred-200">closed</span>
                 </span>
             </div>
         </>
