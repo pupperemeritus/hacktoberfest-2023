77736523988bc89ac23b3b407e5eb8b16f2a64ce
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-10-04 22:36:16 +0530

    reduced text size in mobile devices

--- components/FormClosed.jsx
+++ components/FormClosed.jsx
@@ -4,7 +4,7 @@ const FormClosed = () => {
     return (
         <>
             <div className="px-8 py-4 mx-4 font-bold md:mx-60 my-60 glass h-min">
-                <span className="text-xl md:text-4xl">
+                <span className="text-lg md:text-4xl">
                     <span className=" text-bgold-200">Registrations are</span>{" "}
                     <span className=" text-bred-200">closed</span>
                 </span>
