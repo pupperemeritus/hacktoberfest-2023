76141a7f7154fd35cb7d53bcfbde5ff3f3277589
Sai Kiran <98962380+SaiKiranMatta@users.noreply.github.com>
2024-10-09 18:21:18 +0530

    temporary form open

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,12 +1,12 @@
-// "use client";
-// import Form from "@/components/Form";
+"use client";
+import Form from "@/components/Form";
 import FormClosed from "@/components/FormClosed";
 
 export default function Home() {
     return (
         <main className="flex justify-center w-full spacer layer8">
-            {/* <Form /> */}
-            <FormClosed />
+            <Form /> 
+            {/*<FormClosed />*/}
         </main>
     );
 }
